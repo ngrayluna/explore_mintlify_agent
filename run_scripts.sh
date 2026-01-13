a40f54b9837396d6d890df0218bf7e3b57d56dd4
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2026-01-13 13:47:25 -0800

    temp

--- run_scripts.sh
+++ run_scripts.sh
@@ -2,17 +2,17 @@
 
 RAW_CSV=OriginalDatasets/chat-export-wb-21fd5541-2020-01-01T24_00_00-2026-01-12T24_00_00.csv
 PROCESSED_DATASET_DIR=evaluate/ProcessedDatasets
-PROCESSED_DATASET_FULL=$PROCESSED_DATASET_DIR/processed_mintlify_dataset_$(date +%F).csv
+PROCESSED_DATASET_FILENAME=processed_mintlify_dataset_$(date +%F).csv
+EVALUATION_FILENAME=evaluation_results_$(date +%F).txt
 
-# echo "Running dataset processing script..."
+echo "Processing dataset..."
 
-# python process_dataset.py --input_file $RAW_CSV --output_dir $PROCESSED_DATASET_DIR
+python process_dataset.py --input_file $RAW_CSV --output_dir $PROCESSED_DATASET_DIR
 
 # echo "Running evaluation script..."
 
-echo "Evaluating dataset located at: $PROCESSED_DATASET_FULL"
+echo "Evaluating dataset located at: $PROCESSED_DATASET_DIR/$PROCESSED_DATASET_FILENAME"
 
-python ./evaluate/evaluate_mint_agent.py --input_file $PROCESSED_DATASET_FULL --output_file $PROCESSED_DATASET_DIR/evaluation_results_$(date +%F).txt
+#python ./evaluate/evaluate_mint_agent.py --input_file $PROCESSED_DATASET_DIR/$PROCESSED_DATASET_FILENAME --output_file $PROCESSED_DATASET_DIR/$EVALUATION_FILENAME
 
-
-python ./evaluate/analyze_judge_output.py --processed_dataset $PROCESSED_DATASET_FULL --judge_output $PROCESSED_DATASET_DIR/evaluation_results_$(date +%F).txt
+python ./evaluate/analyze_judge_output.py --processed_dataset $PROCESSED_DATASET_DIR/$PROCESSED_DATASET_FILENAME --judge_output $PROCESSED_DATASET_DIR/$EVALUATION_FILENAME
